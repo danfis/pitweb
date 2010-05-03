0bf15ccefc092ff31c6ab4f8e94f75ed1e84fc1d
Daniel Fiser <danfis@danfis.cz>
2010-05-03 17:03:36 +0200

    Added README file.

--- index.py
+++ index.py
@@ -6,6 +6,6 @@ from mod_python import apache, util
 import pitweb
 
 def handler(req):
-    parent_dir = '/some/path'
+    parent_dir = '/path/to/dir/with/git/repositories'
     prj_list = pitweb.ProjectListDir(req, parent_dir)
     return prj_list.run()
